dc7856e535cc27ad8573ec7551f5cafe24d33b96
nardinegergis <nardinegergis@gmail.com>
2021-07-27 07:17:55 -0400

    hello world

---
+++ Hello.java
@@ -0,0 +1,22 @@
+/**
+ *@author Nardine
+ *email: nardinegergis@gmail.com
+ *date: 07/26/2021
+ *purpose: My first Hello World program in an IDE
+ */
+
+package com.mthree.helloworld;
+
+/**
+ *
+ * @author hp
+ */
+  public class Hello {
+      
+  
+     public static void main (String[] args){
+
+        System.out.println("Hello, World!");
+        System.out.println("My name is nardine!");
+}
+  }
