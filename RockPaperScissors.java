1c1f7418aad2b4cf0bf258f41b6f2ef2f0d2f8b6
nardinegergis <nardinegergis@gmail.com>
2021-07-28 09:34:21 -0400

    adjusted

--- RockPaperScissors.java
+++ RockPaperScissors.java
@@ -18,20 +18,19 @@ public class RockPaperScissors {
         Scanner scanner = new Scanner(System.in);
         
         while(true){
-            /* create string array for rock papers an scissors */
+            /*create a random array for rock , paper an scissors*/
         String[] rps = {"r" , "p" , "s"};
-        /**we create string computer move and 
-         * we want get a random computer move between an array from 0 to 1 for r , p ,s and 
-         * we pass 3 which is the length of the array*/
+        /*create a random array from 0 to 1 for r,p and s*/
         String computerMove = rps[new Random().nextInt(rps.length)];
         
-        
+        /* define the playermove for the player or the user*/
         String playerMove;
         
         while(true){
-       
         System.out.println("please enter your move(r,p or s)");
+        
         playerMove = scanner.nextLine();
+        /* if the player choose r or p or s*/
         
         if(playerMove.equals("r")||playerMove.equals("p")||playerMove.equals("s")){
         
@@ -41,27 +40,29 @@ public class RockPaperScissors {
         System.out.println(playerMove + " is not a valid move.");
         
         }
-        
+        /*check what the computer played or chose*/
         System.out.println("computer played: " + computerMove);
         
+        /* if the player move equals to the computer chose it said that the gams was tie*/
         if (playerMove.equals(computerMove)) {
             
             System.out.println("the game was a tie");
         }
+        /*if the player chose rock and the computer chose paper then then the player lose the game*/
         else if ( playerMove.equals("r")){
-            if (playerMove.equals("p")){
+            if (computerMove.equals("p")){
                 System.out.println("you lose");
             }else if ( computerMove.equals("s")){
                 System.out.println("you win");
         }
             else if ( playerMove.equals("p")){
-            if (playerMove.equals("r")){
+            if (computerMove.equals("r")){
                 System.out.println("you win");
             }else if ( computerMove.equals("s")){
                 System.out.println("you lose");
         }
             else if ( playerMove.equals("s")){
-            if (playerMove.equals("p")){
+            if (computerMove.equals("p")){
                 System.out.println("you win");
             }else if ( computerMove.equals("r")){
                 System.out.println("you lose");
